0143370be7015a175000031726fc89f9921385cc
Josh Knutson <jknutson@imagetrend.com>
2022-01-12 11:45:24 -0600

    adding file Get-SsasSourceConnectionString

---
+++ DeployCube/public/Get-SsasSourceConnectionString.ps1
@@ -0,0 +1,23 @@
+function Get-SsasSourceConnectionString {
+    [OutputType([string])]
+    [CmdletBinding()]
+    param
+    (
+        [String] [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        $SourceServerName,
+
+        [String] [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        $SourceDatabaseName,
+
+        [String] [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        $ExistingConnectionString
+    )
+
+    $ConnBuilder = New-Object System.Data.OleDb.OleDbConnectionStringBuilder($ExistingConnectionString);
+    $ConnBuilder["Data Source"] = $SourceServerName;
+    $ConnBuilder["Initial Catalog"] = $SourceDatabaseName;
+    return $ConnBuilder.ConnectionString;
+}
